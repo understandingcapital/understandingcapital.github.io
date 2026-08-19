0208ebbb5cd69d4e673522927c8ac29e30d7c617
Hagen Blix <40038070+hagenblix@users.noreply.github.com>
2026-08-19 10:51:13 -0400

    Adding a German Citation Mechanism

--- README.md
+++ README.md
@@ -28,12 +28,3 @@ Check out the [theme's docs](https://github.com/cotes2020/jekyll-theme-chirpy/wi
 ## Contributing
 
 This repository is automatically updated with new releases from the theme repository. If you encounter any issues or want to contribute to its improvement, please visit the [theme repository][chirpy] to provide feedback.
-
-## License
-
-This work is published under [MIT][mit] License.
-
-[gem]: https://rubygems.org/gems/jekyll-theme-chirpy
-[chirpy]: https://github.com/cotes2020/jekyll-theme-chirpy/
-[CD]: https://en.wikipedia.org/wiki/Continuous_deployment
-[mit]: https://github.com/cotes2020/chirpy-starter/blob/master/LICENSE
---
+++ _plugins/german_tag.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class GermanTag < Liquid::Block
+    Syntax = /([^\s]+(?:\s+[^\s]+)*)/
+
+    def initialize(tag_name, markup, tokens)
+      super
+      @cite = nil
+
+      # Parse: {% german cite="MEW 23:119" %}
+      if markup =~ /cite\s*=\s*["']([^"']+)["']/
+        @cite = Regexp.last_match(1)
+      end
+    end
+
+    def render(context)
+      text = super.strip
+
+      site = context.registers[:site]
+      converter = site.find_converter_instance(
+        Jekyll::Converters::Markdown
+      )
+
+      # Render Markdown inside the quotation, if desired.
+      rendered = converter.convert(text).strip
+
+      citation = @cite ? " · #{@cite}" : ""
+
+      <<~HTML
+        <span class="german-note">
+          <span class="german-note-marker"
+                tabindex="0"
+                role="note">
+            <span class="german-note-quote">❝</span><span class="german-note-lang">DE</span><span class="german-note-citation">#{citation}</span>
+            <span class="german-note-popup">#{rendered}</span>
+          </span>
+        </span>
+      HTML
+    end
+  end
+
+  Liquid::Template.register_tag("german", GermanTag)
+end
